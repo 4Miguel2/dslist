7f8f80f7365b8e5c8a174998a8c2ee91471c98e1
4Miguel2 <delgdevoleper@gmail.com>
2023-11-17 16:39:07 -0300

    Homolog ok

--- src/main/java/com/devmig/dslist/projections/GameMinProjection.java
+++ src/main/java/com/devmig/dslist/projections/GameMinProjection.java
@@ -4,7 +4,7 @@ public interface GameMinProjection {
 	
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String GetImg_url();
 	String GetShortDescription();
 	Integer GetPosition();
